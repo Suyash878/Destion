a19be44d5aabe82984c6fc314b72706048dfd65b
suyash <suyashmishra145@gmail.com>
2024-10-14 22:22:01 +0530

    Adding all the files

---
+++ React-development/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Login } from './pages/Login'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
